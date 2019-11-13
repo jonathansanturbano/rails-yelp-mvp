fd80add7d2281e97e7b93debcc55aea264e8cbae
Jonathan Santurbano <jonathan.santurbano@gmail.com>
2019-11-13 15:37:26 +0100

    Cheap version of Yelp

---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review < ApplicationRecord
+  STARS = [0, 1, 2, 3, 4, 5]
+  belongs_to :restaurant
+  validates :restaurant, presence: true
+  validates :content, presence: true
+  validates :rating, numericality: { only_integer: true }, inclusion: { in: STARS }
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+puts 'Creating 5 fake restaurants...'
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Company.name,
+    phone_number: Faker::PhoneNumber.phone_number,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  restaurant.save!
+end
+puts 'Finished!'
